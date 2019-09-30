03f68077e2280d31d0945b63bebceaae82e88fbd
Robert Boutillier <36680316+rbout@users.noreply.github.com>
2019-09-30 18:17:59 -0400

    Added main files

---
+++ src/Appbar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+export default function ButtonAppBar() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title}>
+                        News
+                    </Typography>
+                    <Fab color="primary" aria-label="add" className={classes.fab}>
+                        <AddIcon />
+                    </Fab>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
